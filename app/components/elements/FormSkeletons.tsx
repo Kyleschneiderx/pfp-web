23d299b21d0603d189f3e350fcae4e7c8a0a41bf
g360.michael-at-871707287623 <michael.edano@guerilla360.com>
2024-09-26 21:26:51 +0800

    Added loading skeletons for edit pages.

---
+++ app/components/elements/FormSkeletons.tsx
@@ -0,0 +1,18 @@
+const shimmer =
+  "before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent";
+
+export function FormSkeletons() {
+
+  return (
+    <div className={`${shimmer} relative w-full overflow-hidden md:col-span-4`}>
+      <div className="flex">
+        <div className="mb-4 h-10 w-[550px] rounded-md bg-gray-200" />
+        <div className="ml-auto h-10 w-[200px] rounded-md bg-gray-200" />
+      </div>
+      <div className="flex flex-wrap mt-6">
+      <div className="mb-5 mr-5 w-[500px] h-[550px] rounded-lg bg-gray-200" />
+      <div className="mb-5 mr-5 w-[350px] h-[400px] rounded-lg bg-gray-200" />
+      </div>
+    </div>
+  );
+}
