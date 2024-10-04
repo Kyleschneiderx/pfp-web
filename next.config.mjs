ce00786af3a346e4f9860c4d96e04b36f4a18b92
g360.michael-at-871707287623 <michael.edano@guerilla360.com>
2024-10-04 20:54:53 +0800

    Image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['pelvic-floor-api-alb-155109791.ap-southeast-1.elb.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "pelvic-floor-api-alb-155109791.ap-southeast-1.elb.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
   }
 };
 
