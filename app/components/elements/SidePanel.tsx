ad2523f3da188956d45328e96d5869109344bbb9
g360.michael-at-871707287623 <michael.edano@guerilla360.com>
2024-11-05 13:45:44 +0800

    Fixed the vertical alignment of the button done in side panel

--- app/components/elements/SidePanel.tsx
+++ app/components/elements/SidePanel.tsx
@@ -11,17 +11,17 @@ export default function SidePanel({ isOpen, onClose, children }: Props) {
   return (
     <div
       className={clsx(
-        "fixed top-[80px] right-0 h-[calc(100vh-83px)] w-full sm:w-[450px] bg-white shadow-lg z-50 transform transition-transform duration-300 ease-in-out",
+        "fixed top-0 right-0 h-full w-full sm:w-[450px] bg-white shadow-lg z-50 transform transition-transform duration-300 ease-in-out",
         isOpen ? "translate-x-0" : "translate-x-full"
       )}
     >
       <div className="flex flex-col h-full">
-        <div className="sm:flex-grow overflow-auto p-4">{children}</div>
+        <div className="flex-grow overflow-auto p-4">{children}</div>
         <div className="p-4">
           <Button
             label="Done"
             secondary
-            className="right-4 !py-2 !px-4 ml-auto"
+            className="mr-2 sm:mr-0 !py-2 !px-4 ml-auto"
             onClick={onClose}
           />
         </div>
