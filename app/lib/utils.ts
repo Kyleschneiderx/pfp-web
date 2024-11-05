4509c4ab06522c2572d4431d83186576ffb437cc
g360.michael-at-871707287623 <michael.edano@guerilla360.com>
2024-11-05 09:09:40 +0800

    Fixed date formatting where it deduct 1 day

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -3,7 +3,11 @@ import draftToHtml from "draftjs-to-html";
 
 // return format: 1988-11-23
 export const formatDate = (date: Date): string => {
-  return date.toISOString().split("T")[0];
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
 };
 
 // return format: December 25, 1998
