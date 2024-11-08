cfb2f370e94ad36c4dbcbf04568df5384ba786f7
g360.michael-at-871707287623 <michael.edano@guerilla360.com>
2024-11-08 11:38:12 +0800

    Mobile responsive inhancement on navigation bar

--- app/(admin-panel)/layout.tsx
+++ app/(admin-panel)/layout.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import Navigation from "@/app/components/navigation";
-import clsx from "clsx";
 import dynamic from "next/dynamic";
 import { ReactNode } from "react";
 import NavigationMobile from "../components/navigation-mobile";
-import { useToggle } from "../store/store";
 
 const Header = dynamic(() => import("@/app/components/header"), { ssr: false });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  const { isOpen } = useToggle();
-
   return (
     <div className="flex h-screen">
       <div className="hidden sm:block">
@@ -20,11 +16,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <div className="flex flex-col w-full">
         <Header />
         <div className="relative">
-          <div
-            className={clsx("absolute z-50", !isOpen ? "hidden" : "sm:hidden")}
-          >
-            <NavigationMobile />
-          </div>
+          <NavigationMobile />
           <main className="bg-primary-50 h-[calc(100vh-85px)] relative main-inner-left-shadow p-5 overflow-auto">
             {children}
           </main>
--- app/components/navigation-mobile.tsx
+++ app/components/navigation-mobile.tsx
@@ -50,34 +50,50 @@ export default function NavigationMobile() {
     },
   ];
 
-  const { setIsOpen } = useToggle();
+  const { isOpen, setIsOpen } = useToggle();
 
   return (
-    <aside className="w-[250px] shadow-xl flex flex-col h-[calc(100vh-138px)] bg-white pt-8 z-50">
-      <nav>
-        {navItems.map(({ title, url, icon }, index) => (
-          <Link
-            key={index}
-            href={url}
-            className={clsx(
-              "flex items-center py-4 pl-[35px] hover:bg-primary-50 hover:border-r-4 hover:border-primary-500 group",
-              pathname.startsWith(url) &&
-                "bg-primary-100 border-r-4 border-primary-500"
-            )}
-            onClick={() => setIsOpen(false)}
-          >
-            <span className="mr-3">{icon}</span>
-            <span className="text-neutral-600 font-medium">{title}</span>
-          </Link>
-        ))}
-      </nav>
+    <>
       <div
-        onClick={logout}
-        className="flex items-center pl-9 mt-auto mb-[25px] space-x-2 text-red-400 cursor-pointer"
+        className={clsx(
+          "absolute h-full sm:hidden z-50 left-0 top-0 transform transition-transform duration-300 ease-in-out",
+          isOpen ? "translate-x-0" : "-translate-x-full"
+        )}
       >
-        <LogOut size={16} />
-        <p>Logout</p>
+        <aside className="w-[250px] shadow-xl flex flex-col bg-white pt-8 h-full z-50">
+          <nav className="flex-grow">
+            {navItems.map(({ title, url, icon }, index) => (
+              <Link
+                key={index}
+                href={url}
+                className={clsx(
+                  "flex items-center py-4 pl-[35px] hover:bg-primary-50 hover:border-r-4 hover:border-primary-500 group",
+                  pathname.startsWith(url) &&
+                    "bg-primary-100 border-r-4 border-primary-500"
+                )}
+                onClick={() => setIsOpen(false)}
+              >
+                <span className="mr-3">{icon}</span>
+                <span className="text-neutral-600 font-medium">{title}</span>
+              </Link>
+            ))}
+          </nav>
+          <div
+            onClick={logout}
+            className="flex items-center pl-9 mb-[25px] space-x-2 text-red-400 cursor-pointer"
+          >
+            <LogOut size={16} />
+            <p>Logout</p>
+          </div>
+        </aside>
       </div>
-    </aside>
+
+      {isOpen && (
+        <div
+          className="fixed inset-0 top-[80px] bg-black opacity-50 z-40"
+          onClick={() => setIsOpen(false)}
+        />
+      )}
+    </>
   );
 }
