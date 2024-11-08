8fc9e04125bf560f369b589393f72e81be88fc5b
g360.michael-at-871707287623 <michael.edano@guerilla360.com>
2024-11-08 14:18:35 +0800

    Test link

--- app/mobile-app/education/page.tsx
+++ app/mobile-app/education/page.tsx
@@ -1,14 +1,9 @@
-"use client";
-
-import { useEffect } from "react";
+import Link from "next/link";
 
 export default function Page() {
-  useEffect(() => {
-    // Redirect to the URL automatically when the component loads
-    window.location.href = "https://development.d39k3qcd6esorh.amplifyapp.com/mobile-app/education?id=37";
-  }, []);
-
   return (
-    <p>Redirecting...</p>
+    <Link href="https://development.d39k3qcd6esorh.amplifyapp.com/mobile-app/education?id=37">
+      https://development.d39k3qcd6esorh.amplifyapp.com/mobile-app/education?id=37
+    </Link>
   );
 }
