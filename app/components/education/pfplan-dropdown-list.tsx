e19b3b41295ad79fb9e6543cad4f0f23b9758edd
Allen Poblete <allen.poblete@guerilla360.com>
2025-04-10 12:32:48 +0800

    refactor: add as type in SelectCmp due to array and single now acceptable

--- app/components/education/pfplan-dropdown-list.tsx
+++ app/components/education/pfplan-dropdown-list.tsx
@@ -4,36 +4,36 @@ import { useEffect, useState } from "react";
 import SelectCmp from "../elements/SelectCmp";
 
 interface Props {
-  value: OptionsModel | null;
-  setValue: (data: OptionsModel | null) => void;
-  getOptions: (data: OptionsModel[]) => void;
+	value: OptionsModel | null;
+	setValue: (data: OptionsModel | null) => void;
+	getOptions: (data: OptionsModel[]) => void;
 }
 
 export default function PfPlanDropdownList({ value = null, setValue, getOptions }: Props) {
-  const [options, setOptions] = useState<OptionsModel[]>([]);
+	const [options, setOptions] = useState<OptionsModel[]>([]);
 
-  const fetchPfPlans = async () => {
-    const params = `sort[]=name:ASC&page=1&page_items=1000`;
-    const { data } = await getPfPlans(params);
-    const transformList = data.map((el) => ({
-      label: el.name,
-      value: el.id.toString(),
-    }));
-    setOptions(transformList);
-    getOptions(transformList);
-  };
+	const fetchPfPlans = async () => {
+		const params = `sort[]=name:ASC&page=1&page_items=1000`;
+		const { data } = await getPfPlans(params);
+		const transformList = data.map((el) => ({
+			label: el.name,
+			value: el.id.toString(),
+		}));
+		setOptions(transformList);
+		getOptions(transformList);
+	};
 
-  useEffect(() => {
-    fetchPfPlans();
-  }, []);
+	useEffect(() => {
+		fetchPfPlans();
+	}, []);
 
-  return (
-    <SelectCmp
-      options={options}
-      value={value}
-      onChange={(e) => setValue(e)}
-      placeholder="Select PF Plan"
-      wrapperClassName="z-50"
-    />
-  );
+	return (
+		<SelectCmp
+			options={options}
+			value={value}
+			onChange={(e) => setValue(e as OptionsModel)}
+			placeholder="Select PF Plan"
+			wrapperClassName="z-50"
+		/>
+	);
 }
