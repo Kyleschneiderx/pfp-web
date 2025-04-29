7dca1112985fcc4ee0241ffb04e90cafa8fd8b89
Allen Poblete <allen.poblete@guerilla360.com>
2025-04-29 12:11:38 +0800

    fix: use expiration time from the backend

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -15,105 +15,100 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function Page() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [errors, setErrors] = useState<ValidationErrorModel[]>([]);
-  const [isProcessing, setIsProcessing] = useState<boolean>(false);
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	const [errors, setErrors] = useState<ValidationErrorModel[]>([]);
+	const [isProcessing, setIsProcessing] = useState<boolean>(false);
 
-  const { showSnackBar } = useSnackBar();
-  const router = useRouter();
+	const { showSnackBar } = useSnackBar();
+	const router = useRouter();
 
-  const expireTime = {
-    expires: 1 / 24, // expires in 1 hour
-  };
+	const expireTime = {
+		expires: 1 / 24, // expires in 1 hour
+	};
 
-  const setCookie = (name: string, value: string) => {
-    Cookies.set(name, value, {
-      ...expireTime,
-      sameSite: "Strict",
-      // secure: true, // enable this if the server is already https
-    });
-  };
+	const setCookie = (name: string, value: string, expiresAt: number) => {
+		Cookies.set(name, value, {
+			expires: Math.ceil((expiresAt - Date.now() / 1000) / 3600) / 24,
+			sameSite: "Strict",
+			// secure: true, // enable this if the server is already https
+		});
+	};
 
-  const isValid = () => {
-    const validationErrors = validateForm(email, password);
-    setErrors(validationErrors);
-    return validationErrors.length === 0;
-  };
+	const isValid = () => {
+		const validationErrors = validateForm(email, password);
+		setErrors(validationErrors);
+		return validationErrors.length === 0;
+	};
 
-  useEffect(() => {
-    if (errors.length > 0) {
-      const errorMessages = errors.map((error) => error.message).join("\n");
-      showSnackBar({ message: errorMessages, success: false });
-    }
-  }, [errors]);
+	useEffect(() => {
+		if (errors.length > 0) {
+			const errorMessages = errors.map((error) => error.message).join("\n");
+			showSnackBar({ message: errorMessages, success: false });
+		}
+	}, [errors]);
 
-  const handleLogin = async () => {
-    if (isValid() && !isProcessing) {
-      try {
-        setIsProcessing(true);
-        Cookies.remove("token");
+	const handleLogin = async () => {
+		if (isValid() && !isProcessing) {
+			try {
+				setIsProcessing(true);
+				Cookies.remove("token");
 
-        const response: LoginModel = await login(email, password);
-        setCookie("token", response.token.access);
-        setCookie("user_name", response.user.user_profile.name);
-        setCookie("user_email", response.user.email);
+				const response: LoginModel = await login(email, password);
+				setCookie("token", response.token.access, response.token.expires);
+				setCookie("user_name", response.user.user_profile.name, response.token.expires);
+				setCookie("user_email", response.user.email, response.token.expires);
 
-        window.location.replace("/dashboard");
-        // router.push("/dashboard");
-      } catch (error) {
-        const apiError = error as ErrorModel;
+				window.location.replace("/dashboard");
+				// router.push("/dashboard");
+			} catch (error) {
+				const apiError = error as ErrorModel;
 
-        if (apiError && apiError.msg) {
-          showSnackBar({ message: apiError.msg, success: false });
-        }
-        setIsProcessing(false);
-      }
-    }
-  };
+				if (apiError && apiError.msg) {
+					showSnackBar({ message: apiError.msg, success: false });
+				}
+				setIsProcessing(false);
+			}
+		}
+	};
 
-  const hasError = (fieldName: string) => {
-    return errors.some((error) => error.fieldName === fieldName);
-  };
+	const hasError = (fieldName: string) => {
+		return errors.some((error) => error.fieldName === fieldName);
+	};
 
-  return (
-    <form>
-      <Image
-        src="/images/logo.jpg"
-        alt="Logo"
-        width={172}
-        height={80}
-        quality={100}
-        className="mx-auto w-[172px] h-[80px]"
-        priority
-      />
-      <div className="space-y-5 mt-8">
-        <Input
-          name="email"
-          type="email"
-          placeholder="Email"
-          required
-          invalid={hasError("email")}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <Input
-          name="password"
-          type="password"
-          placeholder="Password"
-          required
-          invalid={hasError("password")}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <Button
-          label="Sign In"
-          className="w-full"
-          onClick={handleLogin}
-          isProcessing={isProcessing}
-        />
-        <Link href="/forgot-password">
-          <p className="text-center mt-7">Forgot Password?</p>
-        </Link>
-      </div>
-    </form>
-  );
+	return (
+		<form>
+			<Image
+				src="/images/logo.jpg"
+				alt="Logo"
+				width={172}
+				height={80}
+				quality={100}
+				className="mx-auto w-[172px] h-[80px]"
+				priority
+			/>
+			<div className="space-y-5 mt-8">
+				<Input
+					name="email"
+					type="email"
+					placeholder="Email"
+					required
+					invalid={hasError("email")}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<Input
+					name="password"
+					type="password"
+					placeholder="Password"
+					required
+					invalid={hasError("password")}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+				<Button label="Sign In" className="w-full" onClick={handleLogin} isProcessing={isProcessing} />
+				<Link href="/forgot-password">
+					<p className="text-center mt-7">Forgot Password?</p>
+				</Link>
+			</div>
+		</form>
+	);
 }
