f84375e24c473d4c045a4ba2215a79538ebbac24
Allen Poblete <allen.poblete@guerilla360.com>
2025-05-06 15:19:15 +0800

    feat: add manage personalize pf plan

--- app/services/server_side/patients.ts
+++ app/services/server_side/patients.ts
@@ -1,37 +1,41 @@
-import {
-  PatientModel,
-  PatientsResponse,
-  PatientSurveyModel,
-  PfPlanProgressModel,
+import type {
+	PatientModel,
+	PatientsResponse,
+	PatientSurveyModel,
+	PfPlanProgressModel,
 } from "@/app/models/patient_model";
+import type { PfPlanModel } from "@/app/models/pfplan_model";
 import { apiServerSide } from "@/app/services/apiServerSide";
 
-export const getPatients = async (
-  params: string
-): Promise<PatientsResponse> => {
-  const url = `/users?${params}`;
-  const data = await apiServerSide({ url: url, method: "GET" });
-  return data as PatientsResponse;
+export const getPatients = async (params: string): Promise<PatientsResponse> => {
+	const url = `/users?${params}`;
+	const data = await apiServerSide({ url: url, method: "GET" });
+	return data as PatientsResponse;
 };
 
 export const getPatientDetails = async (id: string): Promise<PatientModel> => {
-  const url = `/users/${id}`;
-  const data = await apiServerSide({ url: url, method: "GET" });
-  return data as PatientModel;
+	const url = `/users/${id}`;
+	const data = await apiServerSide({ url: url, method: "GET" });
+	return data as PatientModel;
 };
 
-export const getPatientSurvey = async (
-  id: string
-): Promise<PatientSurveyModel[]> => {
-  const url = `/users/${id}/survey`;
-  const data = await apiServerSide({ url: url, method: "GET" });
-  return data as PatientSurveyModel[];
+export const getPatientSurvey = async (id: string): Promise<PatientSurveyModel[]> => {
+	const url = `/users/${id}/survey`;
+	const data = await apiServerSide({ url: url, method: "GET" });
+	return data as PatientSurveyModel[];
 };
 
-export const getPfPlanProgress = async (
-  id: string
-): Promise<PfPlanProgressModel> => {
-  const url = `/users/${id}/pf-plan-progress`;
-  const data = await apiServerSide({ url: url, method: "GET" });
-  return data as PfPlanProgressModel;
+export const getPfPlanProgress = async (id: string): Promise<PfPlanProgressModel> => {
+	const url = `/users/${id}/pf-plan-progress`;
+	const data = await apiServerSide({ url: url, method: "GET" });
+	return data as PfPlanProgressModel;
+};
+
+export const getPersonalizedPfPlan = async (id: string): Promise<PfPlanModel> => {
+	const url = `/users/${id}/pf-plan`;
+	const data = await apiServerSide({
+		url: url,
+		method: "GET",
+	});
+	return data as PfPlanModel;
 };
