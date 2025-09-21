6cacdb92a8b74335b251a7e848d60c020aa171c1
Allen Poblete <allen.poblete@guerilla360.com>
2025-09-21 16:49:23 +0800

    fix: put the call on action

--- app/(admin-panel)/settings/ai-coach/(index)/page.tsx
+++ app/(admin-panel)/settings/ai-coach/(index)/page.tsx
@@ -1,12 +1,14 @@
+import { getAiSettings } from "@/app/components/settings/ai-coach/actions";
 import SettingsAiCoachForm from "@/app/components/settings/ai-coach/settings-ai-coach-form";
 import type { AiCoachSettingsModel } from "@/app/models/settings_models";
-import { getAiCoachSettings } from "@/app/services/server_side/settings";
 
 export default async function Page() {
 	let data: AiCoachSettingsModel = { prompt: "", initiate_prompt: "", max_tokens: "20000" };
 	try {
-		const response = await getAiCoachSettings();
-		data = response.data;
+		const response = await getAiSettings();
+		if (response) {
+			data = response.data;
+		}
 	} catch (error) {
 		console.error(error);
 	}
