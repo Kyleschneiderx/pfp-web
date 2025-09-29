fbf45eb5fa01bcafd9fc52739b7cb3b5a0f0bd3a
Allen Poblete <allen.poblete@guerilla360.com>
2025-09-29 21:46:48 +0800

    feat: implement webrtc on visit room

--- app/components/meeting-room/meeting-lobby.tsx
+++ app/components/meeting-room/meeting-lobby.tsx
@@ -7,6 +7,7 @@ import {
 	ChevronDown,
 	MicIcon,
 	MicOffIcon,
+	PhoneOffIcon,
 	SpeakerIcon,
 	UserIcon,
 	VideoIcon,
@@ -17,28 +18,35 @@ import { formatDate, formatDatetime } from "@/app/lib/utils";
 import ResponsiveActionMenu from "../elements/ResponsiveActionMenu";
 import { useRouter } from "next/navigation";
 import clsx from "clsx";
+import type { Meeting } from "@/app/models/meeting_model";
 
 interface MeetingLobbyProps {
+	meeting: Meeting;
 	onJoin: () => void;
 	stream: MediaStream | null;
 	streamDevices: MediaDeviceInfo[];
 	toggleVideo: () => void;
 	toggleAudio: () => void;
+	onResume: () => void;
 	isVideoOn: boolean;
 	isAudioOn: boolean;
+	isMeetingEnd: boolean;
 	onSelectDevice: (kind: MediaDeviceKind, device: MediaDeviceInfo["deviceId"]) => void;
 	selectedDevices: Record<MediaDeviceKind, MediaDeviceInfo["deviceId"] | undefined>;
 }
 
 export default function MeetingLobby({
+	meeting,
 	stream,
 	streamDevices,
 	selectedDevices,
 	onJoin,
 	isVideoOn,
 	isAudioOn,
+	isMeetingEnd,
 	toggleVideo,
 	toggleAudio,
+	onResume,
 	onSelectDevice,
 }: MeetingLobbyProps) {
 	const videoRef = useRef<HTMLVideoElement>(null);
@@ -49,9 +57,7 @@ export default function MeetingLobby({
 		videoRef.current.srcObject = stream;
 
 		const updateVideoSpeaker = async () => {
-			console.log(1);
 			if (!videoRef.current || !selectedDevices.audiooutput) return;
-			console.log(2);
 			if (typeof videoRef.current.setSinkId === "function") {
 				try {
 					await videoRef.current.setSinkId(selectedDevices.audiooutput);
@@ -68,192 +74,205 @@ export default function MeetingLobby({
 
 	return (
 		<div className="flex flex-col items-center px-12 mt-12">
-			<div className="flex flex-col space-y-1">
-				<Card className="!p-0">
-					<div className="relative overflow-hidden rounded-tl-lg rounded-tr-lg !bg-neutral-900 !max-w-[600px] !max-h-[350px] min-w-[600px] min-h-[350px]">
-						<video
-							ref={videoRef}
-							autoPlay
-							playsInline
-							muted={false}
-							className="w-full h-full object-cover scale-125 origin-center"
-						/>
-						<div className="flex flex-row w-full items-center absolute bottom-0 text-neutral-100 p-3 justify-center space-x-3">
-							<Button
-								onClick={toggleVideo}
-								className="!rounded-full border-2 border-neutral-200 h-12 w-12 !p-0  hover:text-neutral-50 hover:border-neutral-50"
-							>
-								{isVideoOn ? <VideoIcon className="h-6 w-6" /> : <VideoOffIcon className="h-6 w-6" />}
-							</Button>
-
-							<Button
-								onClick={toggleAudio}
-								className="!rounded-full border-2 border-neutral-200 h-12 w-12 !p-0  hover:text-neutral-50 hover:border-neutral-50"
-							>
-								{isAudioOn ? <MicIcon className="h-6 w-6" /> : <MicOffIcon className="h-6 w-6" />}
-							</Button>
-						</div>
+			{isMeetingEnd ? (
+				<div className="flex flex-col h-full w-full justify-center items-center space-y-10 text-neutral-900">
+					<div className="rounded-full border-[6px] border-error-400 p-6">
+						<PhoneOffIcon className="w-16 h-16 text-error-400" />
 					</div>
-					<div className="flex flex-row items-center space-y-3 p-6 text-neutral-900">
-						<div className="flex flex-col">
-							<div className="flex flex-row items-center space-x-2">
-								<UserIcon className="w-5 h-5" />
-								<span className="text-lg ">John Doe</span>
-							</div>
-							<div className="flex flex-row items-center space-x-2">
-								<CalendarIcon className="w-5 h-5" />
-								<span className="text-lg">{formatDatetime(new Date("2025-03-03 22:22:22"), "EEE, MMM d h:mm a")}</span>
-							</div>
-						</div>
-						<div className="flex flex-row items-center space-x-3 ml-auto">
-							<Button label="Leave" onClick={() => window.history.back()} className=" !w-22 !bg-error-400 text-white" />
-							<Button label="Join" onClick={onJoin} className="!w-22" />
-						</div>
+					<p className="text-4xl font-semibold">You left the visit</p>
+					<div className="flex flex-row items-center space-x-3">
+						<Button secondary label="Close" onClick={() => window.close()} />
+						<Button label="Lobby" onClick={onResume} />
 					</div>
-				</Card>
-				<div className="flex flex-row justify-center w-full py-1 space-x-3 text-neutral-900">
-					<ResponsiveActionMenu
-						icon={
-							<div
-								className={`
-                flex flex-row items-center space-x-1 cursor-pointer hover:text-neutral-700 rounded-full px-3
-                border border-transparent hover:border-neutral-300 hover:border
-              `}
-							>
-								<VideoIcon className="w-4 h-4" />
-								<div className="flex items-center">
-									<span className="text-sm truncate w-32">
-										{
-											streamDevices.find(
-												(device) => device.deviceId === stream?.getVideoTracks()[0].getSettings().deviceId,
-											)?.label
-										}
-									</span>
-									<ChevronDown className="w-4 h-4" />
-								</div>
-							</div>
-						}
-						align="center"
-						content={() => {
-							return (
-								<div className="flex flex-col p-1">
-									{streamDevices
-										.filter((device) => device.kind === "videoinput")
-										.map((device) => (
-											<button
-												type="button"
-												key={device.deviceId}
-												className={clsx(
-													"text-left p-3 hover:bg-primary-100 cursor-pointer",
-													device.deviceId === stream?.getVideoTracks()[0].getSettings().deviceId
-														? "bg-primary-100"
-														: "bg-white",
-												)}
-											>
-												{device.label}
-											</button>
-										))}
-								</div>
-							);
-						}}
-					/>
-					<ResponsiveActionMenu
-						icon={
-							<div
-								className={`
-                flex flex-row items-center space-x-1 cursor-pointer hover:text-neutral-700 rounded-full px-3
-                border border-transparent hover:border-neutral-300 hover:border
-              `}
-							>
-								<MicIcon className="w-4 h-4" />
-								<div className="flex items-center">
-									<span className="text-sm truncate w-32">
-										{
-											streamDevices.find(
-												(device) =>
-													device.kind === "audioinput" &&
-													device.deviceId === stream?.getAudioTracks()[0].getSettings().deviceId,
-											)?.label
-										}
-									</span>
-									<ChevronDown className="w-4 h-4" />
-								</div>
-							</div>
-						}
-						align="center"
-						content={() => {
-							return (
-								<div className="flex flex-col p-1">
-									{streamDevices
-										.filter((device) => device.kind === "audioinput")
-										.map((device) => (
-											<button
-												type="button"
-												key={device.deviceId}
-												onClick={() => onSelectDevice(device.kind, device.deviceId)}
-												className={clsx(
-													"text-left p-3 hover:bg-primary-100 cursor-pointer",
-													device.kind === "audioinput" &&
-														device.deviceId === stream?.getAudioTracks()[0].getSettings().deviceId
-														? "bg-primary-100"
-														: "bg-white",
-												)}
-											>
-												{device.label}
-											</button>
-										))}
-								</div>
-							);
-						}}
-					/>
-					<ResponsiveActionMenu
-						icon={
-							<div
-								className={`
-                flex flex-row items-center space-x-1 cursor-pointer hover:text-neutral-700 rounded-full px-3
-                border border-transparent hover:border-neutral-300 hover:border
-              `}
-							>
-								<SpeakerIcon className="w-4 h-4" />
-								<div className="flex items-center">
-									<span className="text-sm truncate w-32">
-										{
-											streamDevices.find(
-												(device) => device.kind === "audiooutput" && device.deviceId === selectedDevices.audiooutput,
-											)?.label
-										}
-									</span>
-									<ChevronDown className="w-4 h-4" />
-								</div>
-							</div>
-						}
-						align="center"
-						content={() => {
-							return (
-								<div className="flex flex-col p-1">
-									{streamDevices
-										.filter((device) => device.kind === "audiooutput")
-										.map((device) => (
-											<button
-												type="button"
-												key={device.deviceId}
-												onClick={() => onSelectDevice(device.kind, device.deviceId)}
-												className={clsx(
-													"text-left p-3 hover:bg-primary-100 cursor-pointer",
-													device.kind === "audiooutput" && device.deviceId === selectedDevices.audiooutput
-														? "bg-primary-100"
-														: "bg-white",
-												)}
-											>
-												{device.label}
-											</button>
-										))}
-								</div>
-							);
-						}}
-					/>
 				</div>
-			</div>
+			) : (
+				<div className="flex flex-col space-y-3">
+					<Card className="!p-0">
+						<div className="relative overflow-hidden rounded-tl-lg rounded-tr-lg !bg-neutral-900 !max-w-[600px] !max-h-[350px] min-w-[600px] min-h-[350px]">
+							<video
+								ref={videoRef}
+								autoPlay
+								playsInline
+								muted={false}
+								className="w-full h-full object-cover scale-125 origin-center"
+							/>
+							<div className="flex flex-row w-full items-center absolute bottom-0 text-neutral-100 p-3 justify-center space-x-3">
+								<Button
+									onClick={toggleVideo}
+									className="!rounded-full border-2 border-neutral-200 h-12 w-12 !p-0  hover:text-neutral-50 hover:border-neutral-50"
+								>
+									{isVideoOn ? <VideoIcon className="h-6 w-6" /> : <VideoOffIcon className="h-6 w-6" />}
+								</Button>
+
+								<Button
+									onClick={toggleAudio}
+									className="!rounded-full border-2 border-neutral-200 h-12 w-12 !p-0  hover:text-neutral-50 hover:border-neutral-50"
+								>
+									{isAudioOn ? <MicIcon className="h-6 w-6" /> : <MicOffIcon className="h-6 w-6" />}
+								</Button>
+							</div>
+						</div>
+						<div className="flex flex-row items-center space-y-3 p-6 text-neutral-900">
+							<div className="flex flex-col">
+								<div className="flex flex-row items-center space-x-2">
+									<UserIcon className="w-5 h-5" />
+									<span className="text-lg ">{meeting.user_profile?.name}</span>
+								</div>
+								<div className="flex flex-row items-center space-x-2">
+									<CalendarIcon className="w-5 h-5" />
+									<span className="text-lg">{formatDatetime(new Date(meeting.starts_at), "EEE, MMM d h:mm a")}</span>
+								</div>
+							</div>
+							<div className="flex flex-row items-center space-x-3 ml-auto">
+								<Button label="Leave" onClick={() => window.close()} className=" !w-22 !bg-error-400 text-white" />
+								<Button label="Join" onClick={onJoin} className="!w-22" />
+							</div>
+						</div>
+					</Card>
+					<div className="flex flex-row justify-center w-full py-1 space-x-3 text-neutral-900">
+						<ResponsiveActionMenu
+							icon={
+								<div
+									className={`
+                flex flex-row items-center space-x-1 cursor-pointer hover:text-neutral-700 rounded-full px-3 p-1
+                border border-transparent hover:border-neutral-300 hover:border
+              `}
+								>
+									<VideoIcon className="w-4 h-4" />
+									<div className="flex items-center">
+										<span className="text-sm truncate w-32">
+											{
+												streamDevices.find(
+													(device) => device.deviceId === stream?.getVideoTracks()[0].getSettings().deviceId,
+												)?.label
+											}
+										</span>
+										<ChevronDown className="w-4 h-4" />
+									</div>
+								</div>
+							}
+							align="center"
+							content={() => {
+								return (
+									<div className="flex flex-col p-1">
+										{streamDevices
+											.filter((device) => device.kind === "videoinput")
+											.map((device) => (
+												<button
+													type="button"
+													key={device.deviceId}
+													className={clsx(
+														"text-left p-3 hover:bg-primary-100 cursor-pointer",
+														device.deviceId === stream?.getVideoTracks()[0].getSettings().deviceId
+															? "bg-primary-100"
+															: "bg-white",
+													)}
+												>
+													{device.label}
+												</button>
+											))}
+									</div>
+								);
+							}}
+						/>
+						<ResponsiveActionMenu
+							icon={
+								<div
+									className={`
+                flex flex-row items-center space-x-1 cursor-pointer hover:text-neutral-700 rounded-full px-3 p-1
+                border border-transparent hover:border-neutral-300 hover:border
+              `}
+								>
+									<MicIcon className="w-4 h-4" />
+									<div className="flex items-center">
+										<span className="text-sm truncate w-32">
+											{
+												streamDevices.find(
+													(device) =>
+														device.kind === "audioinput" &&
+														device.deviceId === stream?.getAudioTracks()[0].getSettings().deviceId,
+												)?.label
+											}
+										</span>
+										<ChevronDown className="w-4 h-4" />
+									</div>
+								</div>
+							}
+							align="center"
+							content={() => {
+								return (
+									<div className="flex flex-col p-1">
+										{streamDevices
+											.filter((device) => device.kind === "audioinput")
+											.map((device) => (
+												<button
+													type="button"
+													key={device.deviceId}
+													onClick={() => onSelectDevice(device.kind, device.deviceId)}
+													className={clsx(
+														"text-left p-3 hover:bg-primary-100 cursor-pointer",
+														device.kind === "audioinput" &&
+															device.deviceId === stream?.getAudioTracks()[0].getSettings().deviceId
+															? "bg-primary-100"
+															: "bg-white",
+													)}
+												>
+													{device.label}
+												</button>
+											))}
+									</div>
+								);
+							}}
+						/>
+						<ResponsiveActionMenu
+							icon={
+								<div
+									className={`
+                flex flex-row items-center space-x-1 cursor-pointer hover:text-neutral-700 rounded-full px-3 p-1
+                border border-transparent hover:border-neutral-300 hover:border
+              `}
+								>
+									<SpeakerIcon className="w-4 h-4" />
+									<div className="flex items-center">
+										<span className="text-sm truncate w-32">
+											{
+												streamDevices.find(
+													(device) => device.kind === "audiooutput" && device.deviceId === selectedDevices.audiooutput,
+												)?.label
+											}
+										</span>
+										<ChevronDown className="w-4 h-4" />
+									</div>
+								</div>
+							}
+							align="center"
+							content={() => {
+								return (
+									<div className="flex flex-col p-1">
+										{streamDevices
+											.filter((device) => device.kind === "audiooutput")
+											.map((device) => (
+												<button
+													type="button"
+													key={device.deviceId}
+													onClick={() => onSelectDevice(device.kind, device.deviceId)}
+													className={clsx(
+														"text-left p-3 hover:bg-primary-100 cursor-pointer",
+														device.kind === "audiooutput" && device.deviceId === selectedDevices.audiooutput
+															? "bg-primary-100"
+															: "bg-white",
+													)}
+												>
+													{device.label}
+												</button>
+											))}
+									</div>
+								);
+							}}
+						/>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
--- app/components/meeting-room/meeting-room.tsx
+++ app/components/meeting-room/meeting-room.tsx
@@ -1,29 +1,27 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import MediaArea from "./media-area";
 import { useModal } from "@/app/contexts/ModalContext";
 import MediaControls from "./media-controls";
 import MeetingRoomSidePanel from "../modals/meeting-room-side-panel";
 import Button from "../elements/Button";
-import { ArrowLeftIcon, InfoIcon, MenuIcon, PhoneIcon } from "lucide-react";
+import { PhoneIcon } from "lucide-react";
 import MeetingLobby from "./meeting-lobby";
 import Card from "../elements/Card";
-import Image from "next/image";
-import ArrowLeft from "@/public/svg/arrow-left.svg";
 import clsx from "clsx";
 import type { Meeting } from "@/app/models/meeting_model";
-
-type ConnectionStatus = "connecting" | "connected" | "disconnected";
+import socketClient from "@/app/services/socket-client";
 
 export default function MeetingRoom({ meeting }: { meeting: Meeting }) {
 	const [isVideoOn, setIsVideoOn] = useState(true);
 	const [isAudioOn, setIsAudioOn] = useState(true);
 	const [isCallActive, setIsCallActive] = useState(false);
+	const [isMeetingEnd, setIsMeetingEnd] = useState(false);
 	const [isScreenSharing, setIsScreenSharing] = useState(false);
-	const [sessionDuration, setSessionDuration] = useState(0);
-	const [connectionStatus, setConnectionStatus] = useState<ConnectionStatus>("disconnected");
 	const [localStream, setLocalStream] = useState<MediaStream | null>(null);
+	const [remoteStream, setRemoteStream] = useState<MediaStream>();
+	const iceServersRef = useRef<RTCConfiguration["iceServers"]>();
 	const [selectedDevices, setSelectedDevices] = useState<
 		Record<MediaDeviceKind, MediaDeviceInfo["deviceId"] | undefined>
 	>({
@@ -32,7 +30,10 @@ export default function MeetingRoom({ meeting }: { meeting: Meeting }) {
 		videoinput: undefined,
 	});
 	const streamDevicesRef = useRef<MediaDeviceInfo[]>([]);
+	const peerConnectionRef = useRef<RTCPeerConnection>();
 	const modal = useModal();
+
+	const meetingSocket = useMemo(() => socketClient({ namespace: "meeting" }), []);
 
 	const cleanupStream = () => {
 		if (localStream) {
@@ -42,7 +43,10 @@ export default function MeetingRoom({ meeting }: { meeting: Meeting }) {
 			}
 			setLocalStream(null);
 		}
-		setConnectionStatus("disconnected");
+
+		if (peerConnectionRef.current) {
+			peerConnectionRef.current.close();
+		}
 	};
 
 	const initializeWebRTC = async () => {
@@ -69,21 +73,86 @@ export default function MeetingRoom({ meeting }: { meeting: Meeting }) {
 			}
 
 			setLocalStream(stream);
-
-			// Simulate connection establishment
-			setTimeout(() => {
-				setConnectionStatus("connected");
-			}, 2000);
 		} catch (error) {
 			console.error("Error accessing media devices:", error);
 		}
+	};
+
+	const initializePeerConnection = (iceServers: RTCConfiguration["iceServers"]) => {
+		peerConnectionRef.current = new RTCPeerConnection({ iceServers });
+
+		if (localStream) {
+			for (const track of localStream.getTracks()) {
+				peerConnectionRef.current.addTrack(track, localStream);
+			}
+		}
+
+		// Send ICE candidates to other peer via signaling
+		peerConnectionRef.current.addEventListener("icecandidate", (e) => {
+			if (!e.candidate) return;
+
+			meetingSocket.emit("candidate", { roomId: meeting.id, candidate: e.candidate });
+		});
+
+		// Remote track(s) -> attach to remoteVideo
+		peerConnectionRef.current.addEventListener("track", (ev) => {
+			// Most browsers provide ev.streams[0]
+			setRemoteStream((prev) => {
+				let remoteStream = ev?.streams?.[0];
+				if (!remoteStream) {
+					const remoteMediaStream = new MediaStream();
+					remoteMediaStream.addTrack(ev.track);
+					remoteStream = remoteMediaStream;
+				}
+				return remoteStream;
+			});
+		});
+
+		// Optional: connection state updates
+		peerConnectionRef.current.addEventListener("connectionstatechange", () => {
+			console.log("peer connection change", peerConnectionRef.current?.connectionState);
+		});
 	};
 
 	// Initialize WebRTC connection
 	useEffect(() => {
 		initializeWebRTC();
 
-		return () => cleanupStream();
+		meetingSocket.connect();
+
+		meetingSocket.on("ice_servers", (data) => {
+			console.log("ice servers", data);
+			iceServersRef.current = data;
+		});
+
+		meetingSocket.on("offer", async ({ from, sdp }) => {
+			if (!peerConnectionRef.current) return;
+			await peerConnectionRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
+			const answer = await peerConnectionRef.current.createAnswer();
+			await peerConnectionRef.current.setLocalDescription(answer);
+			meetingSocket.emit("answer", { roomId: meeting.id, sdp: answer });
+		});
+
+		meetingSocket.on("answer", async ({ from, sdp }) => {
+			if (!peerConnectionRef.current) return;
+			await peerConnectionRef.current.setRemoteDescription(new RTCSessionDescription(sdp));
+		});
+
+		meetingSocket.on("candidate", async ({ candidate }) => {
+			if (!peerConnectionRef.current) return;
+			if (!candidate) return;
+			await peerConnectionRef.current.addIceCandidate(new RTCIceCandidate(candidate));
+		});
+
+		meetingSocket.on("server_error", (data) => {
+			console.error("[MEETING SOCKET]:", data);
+		});
+
+		return () => {
+			meetingSocket.disconnect();
+
+			cleanupStream();
+		};
 	}, []);
 
 	// Handle video/audio toggle
@@ -116,43 +185,45 @@ export default function MeetingRoom({ meeting }: { meeting: Meeting }) {
 		updateMediaStream();
 	}, [selectedDevices]);
 
-	// Session timer
-	useEffect(() => {
-		let interval: NodeJS.Timeout;
-		if (isCallActive) {
-			interval = setInterval(() => {
-				setSessionDuration((prev) => prev + 1);
-			}, 1000);
-		}
-		return () => clearInterval(interval);
-	}, [isCallActive]);
-
-	const formatDuration = (seconds: number) => {
-		const mins = Math.floor(seconds / 60);
-		const secs = seconds % 60;
-		return `${mins.toString().padStart(2, "0")}:${secs.toString().padStart(2, "0")}`;
-	};
-
 	const handleEndCall = () => {
 		cleanupStream();
 		setIsCallActive(false);
+		setIsMeetingEnd(true);
 	};
 
-	const toggleSidePanel = () => {
-		modal.open({
-			type: "panel",
-			overlay: false,
-			component: ({ close }) => <MeetingRoomSidePanel meeting={meeting} onClose={close} />,
+	const handleResume = () => {
+		initializeWebRTC();
+		setIsMeetingEnd(false);
+	};
+
+	const handleJoin = () => {
+		if (!iceServersRef.current) return;
+
+		initializePeerConnection(iceServersRef.current);
+
+		meetingSocket.emit("join", { roomId: meeting.id }, async (response) => {
+			if (response !== "ok") return;
+
+			if (!peerConnectionRef.current) return;
+
+			const offer = await peerConnectionRef.current.createOffer();
+			await peerConnectionRef.current.setLocalDescription(offer);
+			meetingSocket.emit("offer", { roomId: meeting.id, sdp: offer });
 		});
+
+		setIsCallActive(true);
 	};
 
 	return !isCallActive ? (
 		<MeetingLobby
+			meeting={meeting}
 			isVideoOn={isVideoOn}
 			isAudioOn={isAudioOn}
+			isMeetingEnd={isMeetingEnd}
 			toggleVideo={() => setIsVideoOn((prev) => !prev)}
 			toggleAudio={() => setIsAudioOn((prev) => !prev)}
-			onJoin={() => setIsCallActive(true)}
+			onResume={handleResume}
+			onJoin={handleJoin}
 			stream={localStream}
 			streamDevices={streamDevicesRef.current}
 			selectedDevices={selectedDevices}
@@ -179,12 +250,13 @@ export default function MeetingRoom({ meeting }: { meeting: Meeting }) {
 						isCallActive={isCallActive}
 						isScreenSharing={isScreenSharing}
 						stream={localStream}
+						remoteStream={remoteStream}
 						roomId={"11111-111-1111"}
 						className="flex flex-1"
 					/>
 					<div className="p-4 flex flex-row  items-end ">
 						<div className="flex items-center flex-1">
-							<Button
+							{/* <Button
 								onClick={() => handleEndCall()}
 								className={clsx(
 									"!rounded-full h-12 w-12  !p-0",
@@ -194,16 +266,13 @@ export default function MeetingRoom({ meeting }: { meeting: Meeting }) {
 								)}
 							>
 								<PhoneIcon className="h-6 w-6" />
-							</Button>
+							</Button> */}
 							<MediaControls
 								isVideoOn={isVideoOn}
 								isAudioOn={isAudioOn}
-								isCallActive={isCallActive}
-								isScreenSharing={isScreenSharing}
+								onEndCall={handleEndCall}
 								onToggleVideo={() => setIsVideoOn((prev) => !prev)}
-								onToggleCall={() => setIsCallActive((prev) => !prev)}
 								onToggleAudio={() => setIsAudioOn((prev) => !prev)}
-								onToggleScreenSharing={() => setIsScreenSharing((prev) => !prev)}
 							/>
 						</div>
 					</div>
