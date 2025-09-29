5c2e529685b40bbb1192069c5f218e5b9602d989
Allen Poblete <allen.poblete@guerilla360.com>
2025-09-29 21:46:17 +0800

    refactor: separate meeting status management request

--- app/models/meeting_model.ts
+++ app/models/meeting_model.ts
@@ -27,6 +27,12 @@ export interface Meeting {
 
 export interface MeetingForm {
 	id?: number;
-	status_id?: number;
 	soap_notes?: MeetingSoapNotes;
+}
+export interface DraftMeetingForm extends MeetingForm {
+	status_id: 7;
+}
+
+export interface CompleteMeetingForm extends MeetingForm {
+	status_id: 8;
 }
