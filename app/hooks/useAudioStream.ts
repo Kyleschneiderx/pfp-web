9442a41f2ff463ded9c28975863447beeeb96701
Allen Poblete <allen.poblete@guerilla360.com>
2025-10-06 22:45:57 +0800

    feat: add audio recorder hook

--- app/hooks/useAudioStream.ts
+++ app/hooks/useAudioStream.ts
@@ -1,25 +1,120 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function useAudioStream({
 	emit,
+	emitLocal,
+	emitRemote,
 	emitInterval = 5000,
 	localStream,
 	remoteStream,
 	start = false,
 }: {
-	emit: (data: ArrayBuffer) => void;
+	emit?: (data: ArrayBuffer) => void;
+	emitLocal?: (data: ArrayBuffer) => void;
+	emitRemote?: (data: ArrayBuffer) => void;
 	emitInterval?: number;
 	localStream?: MediaStream | null;
 	remoteStream?: MediaStream | null;
 	start?: boolean;
 }) {
 	const audioContextRef = useRef<AudioContext>();
-	const workletNodeRef = useRef<AudioWorkletNode>();
 	const localSourceRef = useRef<MediaStreamAudioSourceNode>();
 	const remoteSourceRef = useRef<MediaStreamAudioSourceNode>();
 	const [workletNode, setWorkletNode] = useState<AudioWorkletNode>();
+	const [localWorkletNode, setLocalWorkletNode] = useState<AudioWorkletNode>();
+	const [remoteWorkletNode, setRemoteWorkletNode] = useState<AudioWorkletNode>();
 	const arrayBufferRef = useRef<Uint8Array[]>([]);
+	const localBufferRef = useRef<Uint8Array[]>([]);
+	const remoteBufferRef = useRef<Uint8Array[]>([]);
 	const emitTimerRef = useRef<NodeJS.Timeout>();
+
+	const onEmit = useCallback((buffer: Uint8Array[], send: (data: ArrayBuffer) => void) => {
+		if (buffer.length <= 0) return;
+
+		const totalLength = buffer.reduce((a, b) => a + b.length, 0);
+		const merged = new Uint8Array(totalLength);
+		let offset = 0;
+		for (const chunk of buffer) {
+			merged.set(chunk, offset);
+			offset += chunk.length;
+		}
+		console.log("emit to backend!");
+		send(merged.buffer);
+	}, []);
+
+	const intervalEmit = () => {
+		if (emit) {
+			onEmit(arrayBufferRef.current, emit);
+			arrayBufferRef.current = [];
+		}
+
+		if (emitLocal) {
+			onEmit(localBufferRef.current, emitLocal);
+			localBufferRef.current = [];
+		}
+
+		if (emitRemote) {
+			onEmit(remoteBufferRef.current, emitRemote);
+			remoteBufferRef.current = [];
+		}
+	};
+
+	const initializeWorklets = () => {
+		if (!audioContextRef.current) return;
+
+		const workletNode = new AudioWorkletNode(audioContextRef.current, "pcm-processor");
+		workletNode.port.onmessage = (event) => {
+			arrayBufferRef.current.push(new Uint8Array(event.data));
+		};
+		setWorkletNode(workletNode);
+
+		const localWorkletNode = new AudioWorkletNode(audioContextRef.current, "pcm-processor");
+		localWorkletNode.port.onmessage = (event) => {
+			localBufferRef.current.push(new Uint8Array(event.data));
+		};
+		setLocalWorkletNode(localWorkletNode);
+
+		const remoteWorkletNode = new AudioWorkletNode(audioContextRef.current, "pcm-processor");
+		remoteWorkletNode.port.onmessage = (event) => {
+			remoteBufferRef.current.push(new Uint8Array(event.data));
+		};
+		setRemoteWorkletNode(remoteWorkletNode);
+	};
+
+	const cleanup = () => {
+		if (emitTimerRef.current) {
+			clearInterval(emitTimerRef.current);
+		}
+
+		if (audioContextRef.current) {
+			audioContextRef.current.close();
+		}
+
+		if (workletNode) {
+			workletNode.disconnect();
+			setWorkletNode(undefined);
+		}
+
+		if (localWorkletNode) {
+			localWorkletNode.disconnect();
+			setLocalWorkletNode(undefined);
+		}
+
+		if (remoteWorkletNode) {
+			remoteWorkletNode.disconnect();
+			setRemoteWorkletNode(undefined);
+		}
+
+		if (localSourceRef.current) {
+			localSourceRef.current.disconnect();
+			localSourceRef.current = undefined;
+		}
+
+		if (remoteSourceRef.current) {
+			remoteSourceRef.current.disconnect();
+			remoteSourceRef.current = undefined;
+		}
+	};
 
 	useEffect(() => {
 		if (!start) return;
@@ -38,30 +133,11 @@ export default function useAudioStream({
 					await audioContextRef.current.resume();
 				}
 
-				const workletNode = new AudioWorkletNode(audioContextRef.current, "pcm-processor");
+				initializeWorklets();
 
-				workletNode.port.onmessage = (event) => {
-					// console.log("Audio data captured:", event.data.byteLength, "bytes");
-					// emit(event.data);
-					arrayBufferRef.current.push(new Uint8Array(event.data));
-				};
-
-				setWorkletNode(workletNode);
-
-				emitTimerRef.current = setInterval(() => {
-					if (arrayBufferRef.current.length > 0) {
-						const totalLength = arrayBufferRef.current.reduce((a, b) => a + b.length, 0);
-						const merged = new Uint8Array(totalLength);
-						let offset = 0;
-						for (const chunk of arrayBufferRef.current) {
-							merged.set(chunk, offset);
-							offset += chunk.length;
-						}
-						arrayBufferRef.current = [];
-						console.log("emit to backend!");
-						emit(merged.buffer);
-					}
-				}, emitInterval);
+				if (emit || emitLocal || emitRemote) {
+					emitTimerRef.current = setInterval(intervalEmit, emitInterval);
+				}
 			} catch (error) {
 				console.error("Failed to initialize audio:", error);
 			}
@@ -71,54 +147,18 @@ export default function useAudioStream({
 	}, [start, emit, emitInterval]);
 
 	useEffect(() => {
-		return () => {
-			if (emitTimerRef.current) {
-				clearInterval(emitTimerRef.current);
-			}
-
-			if (audioContextRef.current) {
-				audioContextRef.current.close();
-			}
-
-			if (workletNode) {
-				workletNode.disconnect();
-				setWorkletNode(undefined);
-			}
-			if (localSourceRef.current) {
-				localSourceRef.current.disconnect();
-				localSourceRef.current = undefined;
-			}
-			if (remoteSourceRef.current) {
-				remoteSourceRef.current.disconnect();
-				remoteSourceRef.current = undefined;
-			}
-		};
-	}, []);
-
-	useEffect(() => {
 		if (start) return;
 
-		if (emitTimerRef.current) {
-			clearInterval(emitTimerRef.current);
-		}
+		intervalEmit();
 
-		if (audioContextRef.current) {
-			audioContextRef.current.close();
-		}
-
-		if (workletNode) {
-			workletNode.disconnect();
-			setWorkletNode(undefined);
-		}
-		if (localSourceRef.current) {
-			localSourceRef.current.disconnect();
-			localSourceRef.current = undefined;
-		}
-		if (remoteSourceRef.current) {
-			remoteSourceRef.current.disconnect();
-			remoteSourceRef.current = undefined;
-		}
+		cleanup();
 	}, [start]);
+
+	useEffect(() => {
+		return () => {
+			cleanup();
+		};
+	}, []);
 
 	useEffect(() => {
 		if (!start) return;
@@ -127,8 +167,6 @@ export default function useAudioStream({
 
 		if (!audioContextRef.current) return;
 
-		if (!workletNode) return;
-
 		if (localSourceRef.current) {
 			localSourceRef.current.disconnect();
 			localSourceRef.current = undefined;
@@ -136,11 +174,18 @@ export default function useAudioStream({
 
 		try {
 			localSourceRef.current = audioContextRef.current.createMediaStreamSource(localStream);
-			localSourceRef.current.connect(workletNode);
+
+			if (workletNode) {
+				localSourceRef.current.connect(workletNode);
+			}
+
+			if (localWorkletNode) {
+				localSourceRef.current.connect(localWorkletNode);
+			}
 		} catch (error) {
 			console.error("Failed to connect local audio source:", error);
 		}
-	}, [localStream, start, workletNode]);
+	}, [localStream, start, localWorkletNode]);
 
 	useEffect(() => {
 		if (!start) return;
@@ -149,8 +194,6 @@ export default function useAudioStream({
 
 		if (!audioContextRef.current) return;
 
-		if (!workletNode) return;
-
 		if (remoteSourceRef.current) {
 			remoteSourceRef.current.disconnect();
 			remoteSourceRef.current = undefined;
@@ -158,9 +201,15 @@ export default function useAudioStream({
 
 		try {
 			remoteSourceRef.current = audioContextRef.current.createMediaStreamSource(remoteStream);
-			remoteSourceRef.current.connect(workletNode);
+			if (workletNode) {
+				remoteSourceRef.current.connect(workletNode);
+			}
+
+			if (remoteWorkletNode) {
+				remoteSourceRef.current.connect(remoteWorkletNode);
+			}
 		} catch (error) {
 			console.error("Failed to connect remote audio source:", error);
 		}
-	}, [remoteStream, start, workletNode]);
+	}, [remoteStream, start, remoteWorkletNode]);
 }
