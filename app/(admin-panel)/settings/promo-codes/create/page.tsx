42a4b93a9ceb285cf1ed42501ba61af5a0cce0ab
Allen Poblete <allen.poblete@guerilla360.com>
2025-11-21 11:11:25 +0800

    chore: remove unknown props

--- app/(admin-panel)/settings/promo-codes/create/page.tsx
+++ app/(admin-panel)/settings/promo-codes/create/page.tsx
@@ -2,5 +2,5 @@ import PatientForm from "@/app/components/patients/patient-form";
 import PromoCodeForm from "@/app/components/promo-codes/promo-code-form";
 
 export default function Page() {
-	return <PromoCodeForm action="Create" />;
+	return <PromoCodeForm />;
 }
