fa3d8f19cd18d0742b217458568d3370f1388720
Allen Poblete <allen.poblete@guerilla360.com>
2026-03-05 11:40:22 +0800

    chore: remove console log

--- app/components/education/actions.ts
+++ app/components/education/actions.ts
@@ -5,24 +5,24 @@ import { ErrorModel } from "@/app/models/error_model";
 import { getEducations } from "@/app/services/server_side/education";
 
 interface Props {
-  page?: number;
-  name: string;
-  sort: string;
+	page?: number;
+	name: string;
+	sort: string;
 }
 
 export async function fetchEducations({
-  page = 1,
-  name, sort,
-}: Props): Promise<{educationList: EducationModel[]; max_page: number}> {
-  let response;
-  const params = `&title=${name}&sort[]=${sort}&page=${page}&page_items=15`;
-  try {
-    response = await getEducations(params);
-    console.log(response.data.length);
-    return { educationList: response.data, max_page: response.max_page};
-  } catch (error) {
-    const apiError = error as ErrorModel;
-    const errorMessage = apiError.msg || "Failed to fetch educations";
-    throw new Error(errorMessage);
-  }
+	page = 1,
+	name,
+	sort,
+}: Props): Promise<{ educationList: EducationModel[]; max_page: number }> {
+	let response;
+	const params = `&title=${name}&sort[]=${sort}&page=${page}&page_items=15`;
+	try {
+		response = await getEducations(params);
+		return { educationList: response.data, max_page: response.max_page };
+	} catch (error) {
+		const apiError = error as ErrorModel;
+		const errorMessage = apiError.msg || "Failed to fetch educations";
+		throw new Error(errorMessage);
+	}
 }
